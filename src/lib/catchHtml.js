7f0e501379d4258a0920b0c4805df92ab2224cc2
CraK <mvcrak@gmail.com>
2020-01-30 19:29:48 +0800

    新添加核心算法

--- src/lib/catchHtml.js
+++ src/lib/catchHtml.js
@@ -1,6 +1,21 @@
 import $ from 'jquery';
 import { flattenDeep } from 'lodash';
 
+// todo default value
+// * 220以下识别为广告
+const MAX_PIC_VALUE = 220;
+// * 400以上为加粗
+const BOLD_WEIGHT = 400;
+
+const lineFeedTags = ["P", "SECTION", "BLOCKQUOTE"];
+
+class ParagraphNode {
+  constructor(type, content = "") {
+    this.type = type;
+    this.content = content;
+  }
+}
+
 // 元素是块(block)还是行(inline)或不可见(false)
 const inlineOrBlock = ($n) => {
   const d = $n.css('display');
@@ -15,7 +30,7 @@ const inlineOrBlock = ($n) => {
 };
 
 // 判断当前元素是不是加粗的
-const isBold = ($n) => $n.css('font-weight') > 400;
+const isBold = ($n) => $n.css('font-weight') > BOLD_WEIGHT;
 
 // 判断当前元素是不是图片
 const isImg = ($n) => $n.is('img')
@@ -23,12 +38,118 @@ const isImg = ($n) => $n.is('img')
 // 获取图片真实的地址（考虑懒加载和防爬）
 const getImgSrc = (img) => {
   const $img = $(img);
-  if (img.width < 220 && img.height < 220) {
+  if (img.width < MAX_PIC_VALUE && img.height < MAX_PIC_VALUE) {
     return '';
   }
   const src = img.src.startsWith('data:image/') || !img.src ? ($img.attr('data-src') || trimCSSURL($img.css('background-image'))) : img.src;
   return src;
 };
+
+// * 第一步，先往下找最底层，取节点
+// * 节点中包括文本及图片
+const analysisWechatNode = (node, total = []) => {
+  if(node.childNodes.length) {
+    node.childNodes.forEach(n => total.concat(analysisWechatNode(n, total)));
+  } else {
+    total.push(node);
+  }
+  
+  return total;
+};
+
+// * 第二步，过滤节点
+// * 只保留文本或图片节点
+const filterNodes = (nodes) => {
+  return nodes.filter(node => (node.tagName === "IMG" || node.textContent.trim()));
+}
+
+// * 第三步，自定义过滤
+// * 过滤某些特定的内容，如小于200的图片，gif等
+// todo 
+
+
+// * 第四步，生成结构
+// * 不同样式等因素造成内容存于不同节点，将其修复
+// todo 微信公众号用于看起来目前用于分端的标签为<p>, <section>, <blockquote>
+const getParagraphNode = (node) => {
+  let result = node;
+  if(!lineFeedTags.includes(node.tagName)) {
+    result = getParagraphNode(node.parentNode)
+  }
+  return result;
+}
+
+const createParagraphContent = (nodes) => {
+  // 存放所有的段落
+  const paragraphPool = [];
+  // 存放段落对应的内容
+  const paragraphContents = [];
+  nodes.forEach(node => {
+    // 获取此节点的分段节点
+    const paragraphNode = getParagraphNode(node);
+    const paragraphIndex = paragraphPool.indexOf(paragraphNode);
+    let paragraphContent = paragraphContents[paragraphIndex];
+    if(paragraphIndex === -1) {
+      paragraphContent = [];
+      paragraphContents.push(paragraphContent)
+      paragraphPool.push(paragraphNode)
+    }
+    paragraphContent.push(node);
+  })
+  return paragraphContents;
+}
+
+// * 第五步，生成DOM
+const getDOMByType = (paragraphNode) => {
+  let dom = "";
+  switch(paragraphNode.type) {
+    case "img": {
+      const p = document.createElement("p");
+      const img = document.createElement("img");
+      img.src = paragraphNode.content;
+      p.appendChild(img);
+      dom = p;
+      break;
+    }
+    case "text": {
+      const p = document.createElement("p");
+      p.innerText = paragraphNode.content;
+      dom = p;
+      break;
+    }
+    default: {
+
+    }
+  }
+  return dom;
+}
+
+const parseDOM = (paragraphContents) => {
+  return paragraphContents.reduce((dom, paragraph) => {
+    // * 获取类型及内容
+    const paragraphNode = paragraph.reduce((content, node) => {
+      if(node.tagName === "IMG") {
+        const imgStr = getImgSrc(node);
+        if(imgStr) {
+          content.type = "img"
+          content.content = imgStr;
+        }
+      } else {
+        content.type = "text";
+        content.content += node.textContent.trim();
+      }
+
+      return content;
+    }, new ParagraphNode());
+
+    // * 根据类型内容生成DOM
+    const nodeDOM = getDOMByType(paragraphNode);
+    if(nodeDOM) {
+      dom.push(nodeDOM);
+    }
+    return dom;
+  }, [])
+}
 
 const analysisChildNodeType = (n, site) => {
   const result = [];
@@ -173,8 +294,15 @@ export const catchHtml = (selector) => {
       message: '没有找到设定的入口',
     }
   }
-  const analysisTree = analysisChildNodeType($entry, configs.site);
-  const parsed = flattenDeep(parseTree(analysisTree, configs, []));
+  const wechatNodes = analysisWechatNode($entry);
+  const articleNodes = filterNodes(wechatNodes);
+  const paragraphContent = createParagraphContent(articleNodes);
+  const parsed = parseDOM(paragraphContent)
+
+  // const analysisTree = analysisChildNodeType($entry, configs.site);
+  // const parsed = flattenDeep(parseTree(analysisTree, configs, []));
+
+
   console.log(parsed);
   const html = parsed.filter(node => !!(node && node.innerHTML)).map(node => {
     return node.outerHTML ? node.outerHTML : node.contentText;
